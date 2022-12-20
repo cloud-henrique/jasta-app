756d86753b1dd96c5e17b16d5912ea5640972776
claudio-guiavet <claudio@guia.vet>
2022-12-20 00:43:03 -0300

    style: tab bar

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,3 +1,5 @@
+import Icon from '@expo/vector-icons/Feather'
+import { useTheme } from 'styled-components/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { Home } from '@screens/Home'
@@ -6,10 +8,28 @@ import { Profile } from '@screens/Profile'
 const { Screen, Navigator } = createBottomTabNavigator()
 
 export function TabRoutes() {
+  const { colors } = useTheme()
+
   return (
-    <Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
-      <Screen name='Home' component={Home} />
-      <Screen name='Profile' component={Profile} />
+    <Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.PRIMARY,
+        tabBarLabelStyle: { fontSize: 12, fontWeight: '500' },
+        tabBarStyle: { backgroundColor: colors.BACKGROUND, height: 88 },
+      }}
+    >
+      <Screen
+        name='Home'
+        component={Home}
+        options={{ tabBarLabel: 'Notas', tabBarIcon: ({ color }) => <Icon name='list' color={color} size={28} /> }}
+      />
+      <Screen
+        name='Profile'
+        component={Profile}
+        options={{ tabBarLabel: 'Perfil', tabBarIcon: ({ color }) => <Icon name='user' color={color} size={28} /> }}
+      />
     </Navigator>
   )
 }
